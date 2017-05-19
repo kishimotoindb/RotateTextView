9de35c338b7ff2ba808c07c58b1ecb1259ed755a
fearlessBear <519376470@qq.com>
2017-05-19 11:04:06 +0800

    Initial commit

---
+++ app/src/main/java/com/cuihaichen/rotatetextview/RotateTextView.java
@@ -0,0 +1,31 @@
+package com.cuihaichen.rotatetextview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by cuihaichen on 17-5-19.
+ */
+
+@SuppressWarnings("all")
+public class RotateTextView extends TextView {
+    public RotateTextView(Context context) {
+        super(context);
+    }
+
+    public RotateTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RotateTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RotateTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+}
